08080be1250b25d53c3824cd41227b79314061ec
Nick Gaswirth <nick@roundhouse-designs.com>
2018-12-08 22:40:56 +0000

    Make directories from script

--- start.sh
+++ start.sh
@@ -3,5 +3,13 @@
 # Start the docker network
 docker network create nginx-proxy
 
+# Create necessary directories
+mkdir -p {\
+	certs, \
+	conf.d, \
+	html, \
+	vhost.d \
+}
+
 # Start the nginx-proxy container
 docker-compose up -d
