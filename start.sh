26f0c1d6a41d08389b86cd90d08e55a97f5bdf29
Nick Gaswirth <nick@roundhouse-designs.com>
2018-12-08 23:23:43 +0000

    Added .bash_aliases for wp-cli alias

--- start.sh
+++ start.sh
@@ -6,5 +6,19 @@ docker network create nginx-proxy
 # Create necessary directories
 mkdir -p certs conf.d html vhost.d
 
+# Add wp-cli alias to ~/.bash_aliases if it doesn't exist
+if [ ! -e ~/.bash_aliases ]; then
+	touch ~/.bash_aliases
+	echo "Added file ~/.bash_aliases"
+fi
+
+if grep -Fq "alias wp-cli=" ~/.bash_aliases; then
+	echo "wp-cli alias present, not adding to ~/.bash_aliases"
+else
+	echo "alias wp-cli='docker-compose run --rm wp-cli_${PWD##*/}'" >> ~/.bash_aliases
+	source ~/.bash_aliases
+	echo "wp-cli alias usage: wp-cli <command/args/opts>"
+fi
+
 # Start the nginx-proxy container
 docker-compose up -d
