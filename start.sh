6793e747d9f7c03376608f032957fc2a5278df2f
Nick Gaswirth <admin@roundhouse-designs.com>
2018-12-17 08:32:55 +0000

    check for dockere network up

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # Start the docker network
-docker network create nginx-proxy
+network_up=$(docker network ls --filter name=nginx-proxy --format yes)
+if [ "${network_up}" != 'up' ]; then
+	echo "Starting docker network nginx-proxy..."
+	docker network create nginx-proxy
+fi
 
 # Create necessary directories
 mkdir -p certs conf.d html vhost.d
@@ -15,7 +19,7 @@ fi
 if grep -Fq "alias wp-cli=" ~/.bash_aliases; then
 	echo "wp-cli alias present, not adding to ~/.bash_aliases"
 else
-	echo 'alias wp-cli="docker-compose run --rm wp-cli_${PWD##*/}"' >> ~/.bash_aliases
+	echo "alias wp-cli='"'docker-compose run --rm wp-cli_${PWD##*/}'"'" >> ~/.bash_aliases
 	source ~/.bash_aliases
 	echo "wp-cli alias usage: wp-cli <command/args/opts>"
 fi
