d90e667455060d0b477c80916d91b90019ed7e92
Nick Gaswirth <nick@roundhouse-designs.com>
2019-01-19 00:59:49 +0000

    Customizable wp-cli alais

--- start.sh
+++ start.sh
@@ -13,16 +13,22 @@ fi
 mkdir -p certs conf.d html vhost.d
 
 # Add wp-cli alias to ~/.bash_aliases if it doesn't exist
-if [ ! -e "${homeDir}/.bash_aliases" ]; then
-	echo "No .bash_aliases file, skipping..."
-else
-	if grep -Fq "alias wp-cli=" "${homeDir}/.bash_aliases"; then
-		echo "wp-cli alias present, not adding to ${homeDir}/.bash_aliases"
+read -p "WP-CLI alias prefix (leave blank to skip): " wpcli_prefix
+if [ ! -z "${wpcli_prefix}" ]; then
+	if [ ! -e "${homeDir}/.bash_aliases" ]; then
+		echo "No .bash_aliases file, skipping..."
 	else
-		echo "alias wp-cli='"'docker-compose run --rm wp-cli_${PWD##*/}'"'" >> "${homeDir}/.bash_aliases"
-		source "${homeDir}/.bash_aliases"
-		echo 'wp-cli alias usage: wp-cli_CONTAINER <command/args/opts>'
+		if grep -Fq "alias ${wpcli_prefix}=" "${homeDir}/.bash_aliases"; then
+			echo "${wpcli_prefix} alias present, not adding to ${homeDir}/.bash_aliases"
+		else
+			echo "alias ${wpcli_alias}='"'docker-compose run --rm wp-cli_${PWD##*/}'"'" >> "${homeDir}/.bash_aliases"
+			source "${homeDir}/.bash_aliases"
+			echo "wp-cli alias usage: ${wpcli_prefix}-cli_CONTAINER <command/args/opts>"
+		fi
 	fi
+
+else
+	echo "No wp-cli alias specified, skipping."
 fi
 
 # Add vports file
